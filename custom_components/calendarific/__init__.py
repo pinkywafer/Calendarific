8a09ba2fda5bc7e33f661bd50e6dd63fa8f0e90c
Snuffy2 <Snuffy2@users.noreply.github.com>
2023-02-21 22:19:44 -0500

    Update __init__.py

--- custom_components/calendarific/__init__.py
+++ custom_components/calendarific/__init__.py
@@ -1,15 +1,22 @@
 """Calendarific Platform"""
-import json
+from datetime import datetime, date
 import logging
-from datetime import date, datetime
-
-import homeassistant.helpers.config_validation as cv
+import json 
 import requests
+
 import voluptuous as vol
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import HomeAssistantType
 
-from .const import DOMAIN
+from homeassistant import config_entries
+import homeassistant.helpers.config_validation as cv
+
+from .const import (
+    DOMAIN,
+    ISSUE_URL,
+    VERSION,
+)
 
 CONF_API_KEY = "api_key"
 CONF_COUNTRY = "country"
@@ -21,7 +28,7 @@ CONFIG_SCHEMA = vol.Schema(
             {
                 vol.Required(CONF_API_KEY): cv.string,
                 vol.Required(CONF_COUNTRY): cv.string,
-                vol.Optional(CONF_STATE, default=""): cv.string,
+                vol.Optional(CONF_STATE, default=''): cv.string
             }
         )
     },
@@ -32,16 +39,17 @@ _LOGGER = logging.getLogger(__name__)
 
 holiday_list = []
 
-
 def setup(hass, config):
     """Set up platform using YAML."""
     if DOMAIN in config:
         api_key = config[DOMAIN].get(CONF_API_KEY)
         country = config[DOMAIN].get(CONF_COUNTRY)
         state = config[DOMAIN].get(CONF_STATE)
-        reader = CalendarificApiReader(api_key, country, state)
+        reader =  CalendarificApiReader(api_key, country, state)
 
-        hass.data[DOMAIN] = {"apiReader": reader}
+        hass.data[DOMAIN] = {
+            'apiReader': reader
+        }
     return True
 
 
@@ -51,59 +59,42 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
     )
     return True
 
-
 async def async_unload_entry(hass, entry):
     """Unload a config entry."""
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
 
-
 class CalendarificApiReader:
+
     def __init__(self, api_key, country, state):
         self._country = country
         self._state = state
         self._api_key = api_key
         self._lastupdated = None
-        _LOGGER.info("apiReader loaded")
+        _LOGGER.debug("apiReader loaded")
         self._holidays = []
         self.next_holidays = []
         self._error_logged = False
         self.update()
-
+    
     def get_state(self):
         return "new"
-
-    def get_date(self, holiday_name):
+    
+    def get_date(self,holiday_name):
         try:
             today = date.today()
-            holiday_datetime = next(
-                i for i in self._holidays if i["name"] == holiday_name
-            )["date"]["datetime"]
-            testdate = date(
-                holiday_datetime["year"],
-                holiday_datetime["month"],
-                holiday_datetime["day"],
-            )
+            holiday_datetime = next(i for i in self._holidays if i['name'] == holiday_name)['date']['datetime']
+            testdate = date(holiday_datetime['year'],holiday_datetime['month'],holiday_datetime['day'])
             if testdate < today:
-                holiday_datetime = next(
-                    i for i in self._next_holidays if i["name"] == holiday_name
-                )["date"]["datetime"]
-                testdate = date(
-                    holiday_datetime["year"],
-                    holiday_datetime["month"],
-                    holiday_datetime["day"],
-                )
+                holiday_datetime = next(i for i in self._next_holidays if i['name'] == holiday_name)['date']['datetime']
+                testdate = date(holiday_datetime['year'],holiday_datetime['month'],holiday_datetime['day'])
             return testdate
-        except Exception as e:
-            _LOGGER.warning("get_date error: " + str(e))
+        except:
             return "-"
-
-    def get_description(self, holiday_name):
+    
+    def get_description(self,holiday_name):
         try:
-            return next(i for i in self._holidays if i["name"] == holiday_name)[
-                "description"
-            ]
-        except Exception as e:
-            _LOGGER.warning("get_description error: " + str(e))
+            return next(i for i in self._holidays if i['name'] == holiday_name)['description']
+        except:
             return "NOT FOUND"
 
     def update(self):
@@ -111,34 +102,31 @@ class CalendarificApiReader:
             return
         self._lastupdated = datetime.now().date()
         year = date.today().year
-        params = {"country": self._country, "year": year, "location": self._state}
+        params = {'country': self._country,'year': year,'location': self._state}
         calapi = calendarificAPI(self._api_key)
         response = calapi.holidays(params)
-        _LOGGER.info("Updating from Calendarific api")
+        _LOGGER.debug("Updating from Calendarific api")
         if 'error' in response:
             if not self._error_logged:
-                _LOGGER.error(response["meta"]["error_detail"])
+                _LOGGER.error(response['meta']['error_detail'])
                 self._error_logged = True
             return
         self._holidays = response['response']['holidays']
-        #_LOGGER.debug("API Holidays: %s" % (self._holidays))
         params['year'] = year + 1
         response = calapi.holidays(params)
-        if "error" in response:
+        if 'error' in response:
             if not self._error_logged:
-                _LOGGER.error(response["meta"]["error_detail"])
+                _LOGGER.error(response['meta']['error_detail'])
                 self._error_logged = True
             return
         self._error_logged = False
         self._next_holidays = response['response']['holidays']
-        #_LOGGER.debug("API Next Holidays: %s" % (self._next_holidays))
         global holiday_list
         holiday_list = []
         for holiday in self._holidays:
-            holiday_list.append(holiday["name"])
-
+            holiday_list.append(holiday['name'])
+        
         return True
-
 
 class calendarificAPI:
     api_key = None
@@ -147,16 +135,16 @@ class calendarificAPI:
         self.api_key = api_key
 
     def holidays(self, parameters):
-        url = "https://calendarific.com/api/v2/holidays?"
+        url = 'https://calendarific.com/api/v2/holidays?'
 
-        if "api_key" not in parameters:
-            parameters["api_key"] = self.api_key
+        if 'api_key' not in parameters:
+            parameters['api_key'] = self.api_key
 
-        response = requests.get(url, params=parameters)
-        data = json.loads(response.text)
+        response = requests.get(url, params=parameters);
+        data     = json.loads(response.text)
 
         if response.status_code != 200:
-            if "error" not in data:
-                data["error"] = "Unknown error."
+            if 'error' not in data:
+                data['error'] = 'Unknown error.'
 
         return data
